7557d89ee938cfddeb242bc2423fa89c4deefdae
Alexander Larsson <alexx.1992@gmail.com>
2015-11-16 16:31:48 +0100

    Added credentials fields

--- ushahidi-post.js
+++ ushahidi-post.js
@@ -2,6 +2,8 @@ module.exports = function(RED) {
     function UshahidiPost(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        var username = this.credentials.username;
+        var password = this.credentials.password;
         this.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
             node.send(msg);
