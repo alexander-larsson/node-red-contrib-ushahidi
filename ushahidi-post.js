9d4c14ad4a3da9bff43d77f2adf30be7ef058faa
Alexander Larsson <alexx.1992@gmail.com>
2015-11-17 12:37:47 +0100

    Added remaining fields and a status message

--- ushahidi-post.js
+++ ushahidi-post.js
@@ -1,19 +1,31 @@
 module.exports = function(RED) {
-    function UshahidiPost(config) {
+  "use strict";
+    function UshahidiAdmin(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         var username = this.credentials.username;
         var password = this.credentials.password;
+        var client_id = this.credentials.client_id;
+        var client_secret = this.credentials.client_secret;
+        var api_endpoint = config.api_endpoint;
+
+        var requestAccessToken = function(){
+          node.status({fill:"blue",shape:"ring",text:"Requesting Access Token"});
+        };
+
+        requestAccessToken();
+
         this.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
             node.send(msg);
         });
     }
-    RED.nodes.registerType("ushahidi-post",UshahidiPost, {
+    RED.nodes.registerType("ushahidi-post",UshahidiAdmin, {
       credentials: {
         client_secret: {type: "text", required: true},
         username: {type: "text", required: true},
         password: {type: "password", required: true},
+        client_id: {type: "text", required: true}
       }
     });
-}
+};
