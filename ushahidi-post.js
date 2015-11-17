79e53e2ee761ad77921f30fa8daf36d7997fdb07
Alexander Larsson <alexx.1992@gmail.com>
2015-11-17 14:46:14 +0100

    Hooks up settings right

--- ushahidi-post.js
+++ ushahidi-post.js
@@ -8,19 +8,29 @@ module.exports = function(RED) {
         var client_id = this.credentials.client_id;
         var client_secret = this.credentials.client_secret;
         var api_endpoint = config.api_endpoint;
+        var method = config.method;
 
         var requestAccessToken = function(){
           node.status({fill:"blue",shape:"ring",text:"Requesting Access Token"});
+
+          node.status({});
         };
 
         requestAccessToken();
 
         this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
+            msg.payload = {
+              "username" : username,
+              "password" : password,
+              "client_id" : client_id,
+              "client_secret" : client_secret,
+              "api_endpoint" : api_endpoint,
+              "method" : method
+            };
             node.send(msg);
         });
     }
-    RED.nodes.registerType("ushahidi-post",UshahidiAdmin, {
+    RED.nodes.registerType("ushahidi-admin",UshahidiAdmin, {
       credentials: {
         client_secret: {type: "text", required: true},
         username: {type: "text", required: true},
